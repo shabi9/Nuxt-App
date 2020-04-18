591cfa6405282c4faaf81814630e33bf8a36f48b
shahbaz Khan <shabi@gmail.com>
2020-04-18 16:47:39 +0300

    Added vuex

--- pages/index.vue
+++ pages/index.vue
@@ -10,26 +10,25 @@
   </div>
 </template>
 <script>
+import { mapState } from 'vuex'
 import EventCard from '@/components/EventCard.vue'
 export default {
   components: {
     EventCard
   },
-  asyncData({ $axios, error }) {
-    return $axios
-      .get('http://localhost:3000/events')
-      .then((Response) => {
-        return {
-          events: Response.data
-        }
+  async fetch({ store, error }) {
+    try {
+      await store.dispatch('events/fetchEvents')
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch events at this time, please try again.'
       })
-      .catch((e) => {
-        error({
-          statusCode: 503,
-          message: 'Unable to fetch events at this time, please try again.'
-        })
-      })
+    }
   },
+  computed: mapState({
+    events: (state) => state.events.events
+  }),
   head() {
     return {
       title: 'Event Listing'
